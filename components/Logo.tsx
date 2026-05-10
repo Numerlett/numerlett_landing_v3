a6142499d8802a1fd4300726c31f893995e88a41
Jayendra Bharti <jay.bharti2804@gmail.com>
2026-05-10 15:18:41 +0530

    Modified Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,19 +9,35 @@ export default function Logo({
   className?: string;
 }) {
   const numerClass =
-    theme === "dark" ? "text-white"
-    : theme === "light" ? "text-primary"
-    : "text-primary dark:text-white";
+    theme === "dark"
+      ? "text-white"
+      : theme === "light"
+        ? "text-primary"
+        : "text-primary dark:text-white";
 
   const lettClass =
-    theme === "dark" ? "text-primary"
-    : theme === "light" ? "text-foreground"
-    : "text-foreground dark:text-primary";
+    theme === "dark"
+      ? "text-primary"
+      : theme === "light"
+        ? "text-foreground"
+        : "text-foreground dark:text-primary";
+
+  const tagLineClass =
+    theme === "dark"
+      ? "text-white"
+      : theme === "light"
+        ? "text-black"
+        : "text-black dark:text-white";
 
   return (
-    <span className={cn("text-4xl", logoFont.className, className)}>
+    <span className={cn("relative text-4xl", logoFont.className, className)}>
       <span className={numerClass}>NUMER</span>
       <span className={lettClass}>LETT</span>
+      <span
+        className={`${tagLineClass} absolute top-4/5 right-0 text-[0.5rem] tracking-widest`}
+      >
+        THE NEXT STEP
+      </span>
     </span>
   );
 }
